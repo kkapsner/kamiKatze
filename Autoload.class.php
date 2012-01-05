b1e78101b6a38c63ed6e3aa9db4fe068b0a023c0
Korbinian kapsner <mail@kkapsner.de>
2012-01-05 17:03:57 +0100

    Added loading points

--- Autoload.class.php
+++ Autoload.class.php
@@ -1,7 +1,16 @@
 <?php
 
 class Autoload {
+	/**
+	 * file extension for a class file
+	 * @var string
+	 */
 	public $classExtension = ".class.php";
+	/**
+	 * file extension for a view file
+	 * @var string
+	 */
+	public $viewExtension = ".view.php";
 	protected $searchPath = array();
 	protected function __construct(){
 		$mainScriptDir = realpath(dirname($_SERVER["SCRIPT_FILENAME"])) . DIRECTORY_SEPARATOR;
@@ -9,7 +18,11 @@ class Autoload {
 		$this->addPath($mainScriptDir . "classes");
 		$this->addPath((dirname(__FILE__)) . DIRECTORY_SEPARATOR . "classes");
 	}
-	
+
+	/**
+	 * The one and only instance.
+	 * @var Autoload
+	 */
 	protected static $instance = null;
 	/**
 	 * Factory method.
@@ -21,7 +34,12 @@ class Autoload {
 		}
 		return self::$instance;
 	}
-	
+
+	/**
+	 * Adds a path to the search array. Checks if the path is correct, points to a directory and if it is not already in the array.
+	 * @param string $path
+	 * @return bool if the path was added
+	 */
 	public function addPath($path){
 		$path = realpath($path);
 		if ($path !== false && is_dir($path) && !in_array($path, $this->searchPath)){
@@ -32,7 +50,11 @@ class Autoload {
 			return false;
 		}
 	}
-	
+
+	/**
+	 * Removes a path form the search array.
+	 * @param string $path
+	 */
 	public function removePath($path){
 		$path = realpath($path) . DIRECTORY_SEPARATOR;
 		$pos = array_search($path, $this->searchPath);
@@ -40,7 +62,12 @@ class Autoload {
 			array_splice($this->searchPath, $pos, 1);
 		}
 	}
-	
+
+	/**
+	 * Searches for a file in the search array paths and returns the found path or false on failure.
+	 * @param type $subpath
+	 * @return mixed the found path or false.
+	 */
 	public function searchFile($subpath){
 		foreach ($this->searchPath as $sPath){
 			if (is_file($sPath . $subpath)){
@@ -49,6 +76,7 @@ class Autoload {
 		}
 		return false;
 	}
+	
 	/**
 	 * Splits a classname into an array. The splits are done at an underscore ("_") or at camelcase positions.
 	 * The underscores are removed and subsequential underscores are counted as one. Beginning and trailing underscores are ignored.
@@ -94,7 +122,17 @@ class Autoload {
 		}
 		return $ret;
 	}
-	
+
+	/**
+	 * The paths to the class files.
+	 * @var array
+	 */
+	public $loadingPoints = array();
+	/**
+	 *
+	 * @param string $classname
+	 * @param string $namespace 
+	 */
 	public function load($classname, $namespace = ""){
 		$split = array_values(
 			array_filter(explode("\\", $namespace . "\\" . $classname))
@@ -116,6 +154,10 @@ class Autoload {
 		}
 		if ($iPath !== false){
 			include_once($iPath);
+			if (!array_key_exists($namespace, $this->loadingPoints)){
+				$this->loadingPoints[$namespace] = array();
+			}
+			$this->loadingPoints[$namespace][$classname] = $iPath;
 		}
 	}
 }
