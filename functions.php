b348d85bdbfcbaee8202754d6c9d9da4133ca675
Korbinian kapsner <mail@kkapsner.de>
2012-05-29 01:29:08 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * deklares some globale funktions
+ * 
+ */
+
+/**
+ * Checkes if a key exists in an array. If it exists the value is returned. Else it returns the value given in $default or false.
+ * @param mixed $key
+ * @param array $array
+ * @param mixed $default
+ * @return mixed
+ */
+function array_read_key($key, $array, $default = false){
+	if (array_key_exists($key, $array)){
+		return $array[$key];
+	}
+	else {
+		return $default;
+	}
+}
+?>
