6d466a25821c9e8f3d7d8db19cec88665fcab439
Korbinian kapsner <mail@kkapsner.de>
2012-05-29 01:28:53 +0200

    Global functions added.

--- autoload.php
+++ autoload.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . "Autoload.class.php");
+include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . "functions.php");
 
 spl_autoload_register(array(Autoload::getInstance(), "load"));
 
