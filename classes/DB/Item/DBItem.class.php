3fd4fc4bf8b0b931d1199110221e335d15f4eff7
Korbinian kapsner <mail@kkapsner.de>
2012-09-28 15:47:05 +0200

    Partly integration of extenders in DBItem

--- classes/DB/Item/DBItem.class.php
+++ classes/DB/Item/DBItem.class.php
@@ -1,14 +1,13 @@
 <?php
-
-/*
- *
- *
+/**
+ * DBItem class definition
  */
 
 /**
- * Description of DBItem
+ * A DBItem instance represents an entry in a DB. To specifiy the table extend this class in a class that has the name of the table.
  *
- * @author kkapsner
+ * @author Korbinian Kapsner
+ * @package DB\Item
  */
 abstract class DBItem extends ViewableHTML{
 	
@@ -30,44 +29,59 @@ abstract class DBItem extends ViewableHTML{
 	 */
 	protected static $extensions = array();
 
-	protected
-		/**
-		 * @var DB
-		 */
-		$db,
-		/**
-		 * @var string
-		 */
-		$table,
-		/**
-		 * @var int
-		 */
-		$DBid = null,
-		/**
-		 * @var bool
-		 */
-		$new = false,
-		/**
-		 * @var bool
-		 */
-		$changed = false,
-		/**
-		 * @var bool
-		 */
-		$changeable = true,
-		/**
-		 * @var bool
-		 */
-		$deleted = false,
-		/**
-		 * @var array
-		 */
-		$oldValues = array(),
-		/**
-		 * @var array
-		 */
-		$newValues = array()
-	;
+	/**
+	 * Instance of the DB
+	 * @var DB
+	 */
+	protected $db;
+
+	/**
+	 * Table name of the items class
+	 * @var string
+	 */
+	protected $table;
+
+	/**
+	 * ID of the item in the DB
+	 * @var int
+	 */
+	protected $DBid = null;
+
+	/**
+	 * If the item is new
+	 * @var bool
+	 */
+	protected $new = false;
+
+	/**
+	 * If the item has changed
+	 * @var bool
+	 */
+	protected $changed = false;
+
+	/**
+	 * If the item can be changed
+	 * @var bool
+	 */
+	protected $changeable = true;
+
+	/**
+	 * If the item has been deleted
+	 * @var bool
+	 */
+	protected $deleted = false;
+
+	/**
+	 * Array of the old values in the DB
+	 * @var array
+	 */
+	protected $oldValues = array();
+
+	/**
+	 * Array of the new values that are set
+	 * @var array
+	 */
+	protected $newValues = array();
 	
 	// static class functions
 
@@ -141,10 +155,10 @@ abstract class DBItem extends ViewableHTML{
 	
 	/**
 	 *
-	 * @param string $class
-	 * @param string $name
-	 * @param string $linkedName
-	 * @param int $linkedId
+	 * @param string $class Classname of the instances that should be received
+	 * @param string $name Name of the field that contains the received instances
+	 * @param string $linkedName Name of the field on the other side
+	 * @param int $linkedId ID of the instance that links to the instances
 	 * @return DBItemCollection with $class
 	 */
 	protected static function getByLinkingTable($class, $name, $linkedName, $linkedId){
@@ -171,7 +185,6 @@ abstract class DBItem extends ViewableHTML{
 	 * @param int $id
 	 */
 	protected static function setInLinkingTable($name, $linkedName, $linkedId, $id){
-		$ret = array();
 		$db = DB::getInstance();
 
 		$table = self::getLinkingTableName($name, $linkedName);
@@ -187,7 +200,6 @@ abstract class DBItem extends ViewableHTML{
 	 * @param int $id
 	 */
 	protected static function removeInLinkingTable($name, $linkedName, $linkedId, $id){
-		$ret = array();
 		$db = DB::getInstance();
 
 		$table = self::getLinkingTableName($name, $linkedName);
@@ -196,9 +208,9 @@ abstract class DBItem extends ViewableHTML{
 	}
 	
 	/**
-	 *
-	 * @param string $class
-	 * @param array $fields
+	 * Creates a new instance of the specific $class with the values in the $fields
+	 * @param string $class Classname of the new instance
+	 * @param array $fields field values
 	 * @return DBItem of type $class 
 	 */
 	public static function createCLASS($class, $fields = array()){
@@ -225,12 +237,19 @@ abstract class DBItem extends ViewableHTML{
 				}
 				else {
 					$keys[] = $db->quote($item->name, DB::PARAM_IDENT);
-					$values[] = $db->quote($fields[$item->name], DB::PARAM_STR);
+					if ($item->null && $fields[$item->name] === ""){
+						$values[] = "NULL";
+					}
+					else {
+						$values[] = $db->quote($fields[$item->name], DB::PARAM_STR);
+					}
 				}
 			}
 		}
 		$db->query("INSERT INTO " . $db->quote(self::$tablePrefix . $class, DB::PARAM_IDENT) . " (" . implode(", ", $keys) . ") VALUES (" . implode(", ", $values) . ")");
-		$item = self::getCLASS($class, $db->lastInsertId());
+		$newId = $db->lastInsertId();
+		#TODO: create extenders
+		$item = self::getCLASS($class, $newId);
 		
 		foreach ($dbItemFields as $name => $value){
 			$item->{$name} = $value;
@@ -284,6 +303,11 @@ abstract class DBItem extends ViewableHTML{
 
 	// class methods
 
+	/**
+	 * Protected constructor. To get an instance of the desired class call DBItem::getCLASS().
+	 * @see DBItem::getCLASS()
+	 * @param int $DBid
+	 */
 	protected function __construct($DBid){
 		$this->db = DB::getInstance();
 		$this->table = $this->db->quote(self::$tablePrefix . get_class($this), DB::PARAM_IDENT);
@@ -291,6 +315,42 @@ abstract class DBItem extends ViewableHTML{
 		$this->load();
 	}
 
+	/**
+	 * Loads the data of an extender
+	 * @param DBItemFieldOption $extenderOption Options that describe the extender.
+	 */
+	private function loadExtender(DBItemFieldOption $extenderOption){
+		$extenderValue = $this->{$extenderOption->name};
+		if ($this->DBid === 0){
+			$data = array();
+			foreach ($extenderOption->extensionFieldOptions[$extenderValue] as $item){
+				/* @var $item DBItemFieldOption */
+				$data[$item->name] = $item->default;
+			}
+			$this->changeable = false;
+		}
+		else {
+			if ($extenderValue !== null){
+				$data = $this->db->query(
+					"SELECT * FROM " .
+					$this->db->quote(self::$tablePrefix . $extenderValue, DB::PARAM_IDENT) .
+					"WHERE `id` = " . $this->DBid
+				);
+				$data = $data->fetch(DB::FETCH_ASSOC);
+				if (!$data){
+					throw new Exception("Invalid database contact administrator. (" . $this->DBid . " not found in extender table " . $extenderValue . ")");
+				}
+			}
+		}
+
+		$this->oldValues = array_merge($this->oldValues, $data);
+		foreach ($extenderOption->extensionFieldOptions[$extenderValue] as $item){
+			/* @var $item DBItemFieldOption */
+			if ($item->extender){
+				$this->loadExtender($item);
+			}
+		}
+	}
 	/**
 	 * Loads the item fresh from the database.
 	 */
@@ -301,7 +361,6 @@ abstract class DBItem extends ViewableHTML{
 				/* @var $item DBItemFieldOption */
 				$data[$item->name] = $item->default;
 			}
-			$this->oldValues = $data;
 			$this->changeable = false;
 		}
 		else {
@@ -310,12 +369,20 @@ abstract class DBItem extends ViewableHTML{
 			if (!$data){
 				throw new Exception("Entry " . $this->DBid . " not found in " . $this->table);
 			}
-			$this->oldValues = $data;
+			
+		}
+		$this->oldValues = $data;
+		foreach (DBItemFieldOption::parseClass(get_class($this)) as $item){
+			/* @var $item DBItemFieldOption */
+			if ($item->extender){
+				$this->loadExtender($item);
+			}
 		}
 	}
 
 	/**
 	 * Saves the item to the database
+	 * @todo save extenders
 	 */
 	public function save(){
 		if ($this->changed && !$this->deleted){
@@ -333,6 +400,7 @@ abstract class DBItem extends ViewableHTML{
 
 	/**
 	 * Deletes the item from the database. Also all connections to the item are removed.
+	 * @todo delete extenders
 	 */
 	public function delete(){
 		if (!$this->deleted){
@@ -362,10 +430,17 @@ abstract class DBItem extends ViewableHTML{
 	
 	/**
 	 * Returns the real value in $oldValues resp. $newValues
-	 * @param string $name
+	 * @param string|DBItemFieldOption $name the name
 	 * @return mixed
 	 */
 	private function getRealValue($name){
+		if ($name instanceof DBItemFieldOption){
+			$defaultReturn = $name->default;
+			$name = $name->name;
+		}
+		else {
+			$defaultReturn = null;
+		}
 		
 		if (array_key_exists($name, $this->newValues)){
 			return $this->newValues[$name];
@@ -373,21 +448,65 @@ abstract class DBItem extends ViewableHTML{
 		if (array_key_exists($name, $this->oldValues)){
 			return $this->oldValues[$name];
 		}
-		
-		throw new InvalidArgumentException("No property " . $name . " found.");
+
+		return $defaultReturn;
+		#throw new InvalidArgumentException("No property " . $name . " found.");
 	}
-	
-	public function __get($name){
-		if ($this->deleted){
-			throw new Exception("Deleted item can not be accessed.");
+
+	/**
+	 * Searches in the field options for a given name. If $searchInAllExtenders is true not only the actual extenders are searched but
+	 * also all potentional extenders.
+	 * @param string $name Name for the field to be searched.
+	 * @param bool $searchInAllExtenders If the search should also include potentional extenders not only the actual ones.
+	 * @param array $options DO NOT USE - only for recursive call with the extenders
+	 * @return DBItemFieldOption The field option to the given $name. If the $name is not found null is returned.
+	 */
+	private function getFieldOption($name, $searchInAllExtenders = false, $options = null){
+		if ($options === null){
+			$options = DBItemFieldOption::parseClass($class);
 		}
-		if ($name === "DBid"){
-			return $this->DBid;
+		foreach ($options as $item){
+			/* @var $item DBItemFieldOption */
+			if ($item->name === $name){
+				return $item;
+			}
+			if ($item->extender){
+				if ($searchInAllExtenders){
+					foreach ($item->typeExtension as $extenderValue){
+						$value = $this->getFieldOption(
+							$name, $searchInAllExtenders, $item->extensionFieldOptions[$extenderValue]
+						);
+						if ($value !== null){
+							return $value;
+						}
+					}
+				}
+				else {
+					$extenderValue = $this->getRealValue($item->name);
+					if ($extenderValue !== null){
+						$value = $this->getFieldOption(
+							$name, $searchInAllExtenders, $item->extensionFieldOptions[$extenderValue]
+						);
+						if ($value !== null){
+							return $value;
+						}
+					}
+				}
+			}
 		}
-		
-		$class = get_class($this);
-		
-		foreach (DBItemFieldOption::parseClass($class) as $item){
+		return null;
+	}
+
+	/**
+	 *
+	 * @param array $options Array of DBItemFieldOptions to be searched in
+	 * @param string $name the name to search for
+	 * @param bool $success if the search was successful
+	 * @return mixed value of the field if found. Null if not found - BUT check $success if the field was found
+	 */
+	private function getBySearchInFieldOptions($options, $name, &$success = true){
+		$success = true;
+		foreach ($options as $item){
 			/* @var $item DBItemFieldOption */
 			if ($item->name === $name){
 				switch ($item->type){
@@ -414,9 +533,53 @@ abstract class DBItem extends ViewableHTML{
 						return $this->getRealValue($name);
 				}
 			}
+			if ($item->extender){
+				$extenderValue = $this->getRealValue($item->name);
+				if ($extenderValue !== null){
+					$value = $this->getBySearchInFieldOptions(
+						$item->extensionFieldOptions[$extenderValue], $name, $success
+					);
+					if ($success){
+						return $value;
+					}
+				}
+				foreach ($item->typeExtension as $possibleExtenderValue){
+					if ($possibleExtenderValue !== $extenderValue){
+						$value = $this->getBySearchInFieldOptions(
+							$item->extensionFieldOptions[$possibleExtenderValue], $name, $success
+						);
+						if ($success){
+							return $value;
+						}
+					}
+				}
+			}
 		}
-		
-		throw new InvalidArgumentException("No property " . $name . " found.");
+		$success = false;
+		return null;
+	}
+
+	public function __get($name){
+		if ($this->deleted){
+			throw new Exception("Deleted item can not be accessed.");
+		}
+		if ($name === "DBid"){
+			return $this->DBid;
+		}
+		/*
+		$class = get_class($this);
+
+		$value = $this->getBySearchInFieldOptions(DBItemFieldOption::parseClass($class), $name, $success);
+		if ($success){
+			return $value;
+		}*/
+		$fieldOption = $this->getFieldOption($name, true);
+		if ($fieldOption === null){
+			throw new InvalidArgumentException("No property " . $name . " found.");
+		}
+		else {
+			return $this->getRealValue($fieldOption);
+		}
 	}
 
 	/**
@@ -434,16 +597,16 @@ abstract class DBItem extends ViewableHTML{
 		}
 	}
 
-	public function __set($name, $value){
-		if ($this->deleted){
-			throw new Exception("Deleted item can not be accessed.");
-		}
-		if (!$this->changeable){
-			throw new Exception("This item can not be changed.");
-		}
-		$class = get_class($this);
-
-		foreach (DBItemFieldOption::parseClass($class) as $item){
+	/**
+	 *
+	 * @param array $options Array of DBItemFieldOptions to be searched in
+	 * @param type $name the name to search for
+	 * @param mixed $value the value to be set
+	 * @param type $success if the search was successful
+	 */
+	private function setBySearchInFieldOptions($options, $name, $value, &$success){
+		$success = true;
+		foreach ($options as $item){
 			/* @var $item DBItemFieldOption */
 			if ($item->name === $name){
 				switch ($item->type){
@@ -536,9 +699,33 @@ abstract class DBItem extends ViewableHTML{
 				#exit foreach-loop AND function
 				return;
 			}
+			if ($item->extender){
+				$extenderValue = $this->getRealValue($item->name);
+				if ($extenderValue !== null){
+					$this->setBySearchInFieldOptions(
+						$item->extensionFieldOptions[$extenderValue], $name, $value, $success
+					);
+					if ($success){
+						return;
+					}
+				}
+			}
 		}
+		$success = false;
+	}
 
-		throw new InvalidArgumentException("No property " . $name . " found.");
+	public function __set($name, $value){
+		if ($this->deleted){
+			throw new Exception("Deleted item can not be accessed.");
+		}
+		if (!$this->changeable){
+			throw new Exception("This item can not be changed.");
+		}
+		$class = get_class($this);
+		$this->setBySearchInFieldOptions(DBItemFieldOption::parseClass($class), $name, $value, $success);
+		if (!$success){
+			throw new InvalidArgumentException("No property " . $name . " found.");
+		}
 	}
 
 	public function __call($name, $arguments){
