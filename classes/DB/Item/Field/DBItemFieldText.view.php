b83b0a2f91d7a2beda593aa1df7a7143b4aaacc3
Korbinian kapsner <mail@kkapsner.de>
2012-10-31 12:37:55 +0100

    Text collapse enabled.

---
+++ classes/DB/Item/Field/DBItemFieldText.view.php
@@ -0,0 +1,46 @@
+<?php
+/* @var $this DBItemField */
+/* @var $args DBItem */
+
+$value = $args->{$this->name};
+if ($value === null){
+	echo "---";
+}
+else {
+	echo "<span>" . $this->html($value) . "</span>";
+	?>
+<script type="text/javascript">
+	(function(){
+		var span = document.getElementsByTagName("span");
+		span = span[span.length - 1];
+		var clientRects = span.getClientRects();
+		if (clientRects.length > 1){
+			var height = (clientRects[0].bottom - clientRects[0].top) + "px";
+			var div = document.createElement("div");
+			div.style.height = height;
+			div.style.overflow = "hidden";
+			div.style.position = "relative";
+			span.parentNode.replaceChild(div, span);
+			div.appendChild(span);
+			
+			var button = document.createElement("div");
+			button.style.cssText = "position: absolute; top: 4px; left: 4px; " +
+				"border: 1px solid; line-height: 0.7em; height: 0.7em; width: 0.7em; cursor: pointer; text-align: center;";
+			button.innerHTML = "+";
+			div.appendChild(button);
+			div.style.paddingLeft = (button.offsetWidth + 8) + "px";
+			button.onclick = function(){
+				if (div.style.height){
+					div.style.height = "";
+					this.innerHTML = "&minus;";
+				}
+				else {
+					div.style.height = height;
+					this.innerHTML = "+";
+				}
+			}
+		}
+	})();
+</script><?php
+}
+?>
