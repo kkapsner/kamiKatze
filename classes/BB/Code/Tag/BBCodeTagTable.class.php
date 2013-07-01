2a2d0dfdec67b7ba0cdad871869552104eb59bad
kkapsner <kkapsner@kk-T520>
2013-07-01 17:10:21 +0200

    Implemented toHTML()-Function.

--- classes/BB/Code/Tag/BBCodeTagTable.class.php
+++ classes/BB/Code/Tag/BBCodeTagTable.class.php
@@ -8,6 +8,7 @@
  *
  * @author Korbinian Kapsner
  * @package BB\Code\Tag
+ * @todo document usage
  */
 class BBCodeTagTable extends BBCodeTag{
 	/**
@@ -36,9 +37,56 @@ class BBCodeTagTable extends BBCodeTag{
 	 * {@inheritdoc}
 	 *
 	 * @return string
-	 * @todo implementation of table to html
 	 */
 	public function toHTML(){
+		if ($this->count()){
+			$ret = "<table";
+			foreach ($this->parameter as $k => $v){
+				if ($v){
+					if ($v === true){
+						$ret .= " " . $k;
+					}
+					else {
+						$ret .= " " . $k . '="'  . htmlentities($v, ENT_QUOTES, $this->getCharset()) . '"';
+					}
+				}
+			}
+			$ret .= "><tr><td>";
+			$len = $this->count() - 1;
+			foreach ($this as $cidx => $child){
+				if (is_a($child, "BBCodeTagText")){
+					$text = $child->toBBCode();
+					if ($cidx === 0){
+						$text = preg_replace("/^\\s+/", "", $text);
+					}
+					if ($cidx === $len){
+						$text = preg_replace("/\\s+$/", "", $child->toBBCode());
+					}
+					$lines = preg_split("/[\\r\\n]+/", $text);
+					foreach ($lines as $i => $line){
+						if ($i){
+							$ret .= "</td></tr><tr><td>";
+						}
+						$cells = preg_split("/(?:\\t+|\\s{2,})/", $line);
+						foreach ($cells as $j => $cell){
+							if ($j){
+								$ret .= "</td><td>";
+							}
+							$ret .= $child->encodeHTML($cell);
+							
+						}
+					}
+				}
+				else {
+					$ret .= $child->toHTML();
+				}
+			}
+			$ret.= "</td></tr></table>";
+			return $ret;
+		}
+		else {
+			return "";
+		}
 		
 	}
 }
