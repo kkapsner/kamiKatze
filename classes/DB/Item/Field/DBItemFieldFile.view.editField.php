4bcface9b69fb4612b1e6364fa6be00f5b1474c6
kkapsner <kkapsner@kk-T520>
2014-01-03 00:43:06 +0100

    Added rename functionality.

--- classes/DB/Item/Field/DBItemFieldFile.view.editField.php
+++ classes/DB/Item/Field/DBItemFieldFile.view.editField.php
@@ -6,6 +6,9 @@
 $postName = $this->getPostName($args);
 $value = $args->{$this->name};
 if ($value !== null){
-	echo "File present (" . $this->html(basename($value->path)) . ")";
+	echo 'File present ' .
+		'<input type="hidden" name="' . $postName . '[fileID]" value="' . $this->html($value->DBid) . '">' .
+		'(<input name="' . $postName . '[filename]" value="'  . $this->html($value->filename) . '"> - clear to delete file)' .
+		'<br>';
 }
-?><input type="file" name="<?php echo $postName;?>"><input type="hidden" name="<?php echo $postName;?>" value="<?php echo $this->html(json_encode($this->getPostName($args, true)));?>">
+?><input type="file" name="<?php echo $postName;?>"><input type="hidden" name="<?php echo $postName;?>[path]" value="<?php echo $this->html(json_encode($this->getPostName($args, true)));?>">
