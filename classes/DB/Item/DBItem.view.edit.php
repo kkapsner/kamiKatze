3895456dded298d6140098d17f975db91b8aec90
kkapsner <kkapsner@kk-T520>
2014-01-02 14:25:15 +0100

    Added events.

--- classes/DB/Item/DBItem.view.edit.php
+++ classes/DB/Item/DBItem.view.edit.php
@@ -3,9 +3,12 @@
 <table class="dbItem">
 	<?php
 	/* @var $this DBItem */
+	$this->emit(new Event("view.edit.fields.start", $this));
 	foreach (DBItemField::parseClass(get_class($this)) as $item){
 		/* @var $item DBItemField */
 		$item->view($context, true, $this);
+		$this->emit(new Event("view.edit.field." . $item->name, $this));
 	}
+	$this->emit(new Event("view.edit.fields.end", $this));
 	?>
 </table>
