54191ca88a81c654f09c905334240b4990292814
kkapsner <kkapsner@kk-T520>
2014-01-03 00:41:41 +0100

    Added rename functionality.

--- classes/DB/Item/Field/DBItemFieldFileItem.class.php
+++ classes/DB/Item/Field/DBItemFieldFileItem.class.php
@@ -22,7 +22,49 @@ class DBItemFieldFileItem extends DBItem{
 	}
 	
 	/**
+	 * Renames the file. If the new filename already exists
+	 * 
+	 * @param String $newName the new file name
+	 */
+	public function rename($newName){
+		if ($newName && $newName !== $this->subpath){
+			$oldPath = $this->path;
+			$dir = dirname($oldPath) . "/";
+			
+			if (file_exists($dir . $newName)){
+				$c = 0;
+				while (file_exists($dir . $c . "/" . $newName)){
+					$c++;
+				}
+				if (!file_exists($dir . $c . "/")){
+					mkdir($dir . $c);
+				}
+				$newName = $c . "/" . $newName;
+			}
+			else {
+				$c = false;
+			}
+			
+			if (rename($oldPath, $dir . $newName)){
+				$this->subpath = dirname($this->subpath) . "/" .
+					(($c !== false)? "": $c . "/") .
+					$newName;
+			}
+		}
+	}
+	
+	/**
+	 * Getter for the files name
+	 * 
+	 * @return String Returns the filename
+	 */
+	public function getFilename(){
+		return basename($this->subpath);
+	}
+	
+	/**
 	 * Getter for the files path.
+	 * 
 	 * @return String Returns the path to the file.
 	 */
 	public function getPath(){
@@ -31,6 +73,7 @@ class DBItemFieldFileItem extends DBItem{
 	
 	/**
 	 * Getter for the files URL.
+	 * 
 	 * @return String Returns the URL to the file.
 	 */
 	public function getURL(){
