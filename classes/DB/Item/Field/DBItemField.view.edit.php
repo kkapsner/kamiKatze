940f6c2b7b9c337b4fab9c4bcc8cbde449fa923d
kkapsner <kkapsner@kk-T520>
2014-01-24 00:54:36 +0100

    Added field name als CSS-Class.

--- classes/DB/Item/Field/DBItemField.view.edit.php
+++ classes/DB/Item/Field/DBItemField.view.edit.php
@@ -4,7 +4,7 @@
 /* @var $args DBItem */
 if ($this->editable){
 	?>
-	<tr>
+	<tr class="<?php echo $this->html($this->name);?>"">
 		<td><?php echo $this->html($this->displayName);?>:</td>
 		<td>
 			<?php $this->view("editField", true, $args);?>
