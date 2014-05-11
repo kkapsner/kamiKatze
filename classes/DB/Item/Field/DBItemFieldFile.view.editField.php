781d447eb1185129dcf56e1d96b3a7800698851c
kkapsner <github@mail.kkapsner.de>
2014-05-11 16:52:52 +0200

    Changed display of DBItemFieldField edit mask

--- classes/DB/Item/Field/DBItemFieldFile.view.editField.php
+++ classes/DB/Item/Field/DBItemFieldFile.view.editField.php
@@ -6,9 +6,8 @@
 $postName = $this->getPostName($args);
 $value = $args->{$this->name};
 if ($value !== null){
-	echo 'File present ' .
-		'<input type="hidden" name="' . $postName . '[fileID]" value="' . $this->html($value->DBid) . '">' .
-		'(<input name="' . $postName . '[filename]" value="'  . $this->html($value->filename) . '"> - clear to delete file)' .
+	echo '<input type="hidden" name="' . $postName . '[fileID]" value="' . $this->html($value->DBid) . '">' .
+		'<input name="' . $postName . '[filename]" value="'  . $this->html($value->filename) . '" title="clear to delete file">' .
 		'<br>';
 }
 ?><input type="file" name="<?php echo $postName;?>"><input type="hidden" name="<?php echo $postName;?>[path]" value="<?php echo $this->html(json_encode($this->getPostName($args, true)));?>">
