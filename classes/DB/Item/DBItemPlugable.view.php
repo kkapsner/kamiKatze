580bf6261ebe8c4cf1ec00a1b803d7f653d9a406
kkapsner <github@mail.kkapsner.de>
2015-09-07 16:10:49 +0200

    Added some markup and CSS-classes to the view of DBItemPlugable.

--- classes/DB/Item/DBItemPlugable.view.php
+++ classes/DB/Item/DBItemPlugable.view.php
@@ -1,17 +1,18 @@
-<?php
+<section class="DBItemPlugable"><?php
 
 /* @var $this DBItemPlugable */
 $this->viewByName("DBItem", $context, true, $args);
 
 $plugins = DBItemPlugable::getPluginsCLASS(get_class($this));
 if (count($plugins)){
-	echo "<h2>Plugins</h2><ul>";
+	echo "<article class=\"Plugins\"><h2>Plugins</h2><ul>";
 	foreach ($plugins as $plugin){
 		/* @var $plugin DBItemPlugin */
 		echo "<li>";
 		$plugin->view(false, true, $this);
 		echo "</li>";
 	}
-	echo "</ul>";
+	echo "</ul></article>";
 }
 ?>
+</section>
