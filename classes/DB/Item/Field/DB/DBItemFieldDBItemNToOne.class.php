6fda308b38221ec253e7e9b70a2ad926af0ad38b
kkapsner <github@mail.kkapsner.de>
2017-05-11 08:22:35 +0200

    DBItem: NtoOne field now does not have to allow null.

--- classes/DB/Item/Field/DB/DBItemFieldDBItemNToOne.class.php
+++ classes/DB/Item/Field/DB/DBItemFieldDBItemNToOne.class.php
@@ -10,4 +10,24 @@
  * @package DB\Item\Field
  */
 class DBItemFieldDBItemNToOne extends DBItemFieldDBItemXToOne{
+	
+
+	/**
+	 * {@inheritdoc}
+	 * 
+	 * @param mixed $value
+	 * @param string $nameOut
+	 * @param string|null $valueOut
+	 */
+	public function appendDBNameAndValueForCreate($value, &$nameOut, &$valueOut = null){
+		DBItemField::appendDBNameAndValueForCreate($value->DBid, $nameOut, $valueOut);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param DBItem $item
+	 * @param array $values
+	 */
+	protected function performAssignmentsAfterCreation(DBItem $item, $values){}
 }
