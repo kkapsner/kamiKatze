0a7bdd6e9a7f0486e1a45f013583b84425736097
kkapsner <github@mail.kkapsner.de>
2020-06-21 02:20:29 +0200

    DBItemFieldTimestamp: fixed edit view on empty value

--- classes/DB/Item/Field/DBItemFieldTimestamp.view.editField.php
+++ classes/DB/Item/Field/DBItemFieldTimestamp.view.editField.php
@@ -1,6 +1,11 @@
 <?php
-/* @var $this DBItemFieldText */
+/* @var $this DBItemFieldTimestamp */
 /* @var $context string */
 /* @var $args DBItem */
 
-?><input name="<?php echo $this->getPostName($args);?>" value="<?php echo $this->html($this->getValue($args)->format(is_array($this->editFormat)? $this->editFormat[0]: $this->editFormat));?>">
+$value = $this->getValue($args);
+if (!$value){
+	$value = new DateTime();
+}
+
+?><input name="<?php echo $this->getPostName($args);?>" value="<?php echo $this->html($value->format(is_array($this->editFormat)? $this->editFormat[0]: $this->editFormat));?>">
