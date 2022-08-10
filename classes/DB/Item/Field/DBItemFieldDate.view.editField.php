286c837f9413687d93a40ea0314e0aeb3ecff1d1
kkapsner <github@mail.kkapsner.de>
2022-08-10 14:05:13 +0200

    DBItemFieldDate required if null not allowed

--- classes/DB/Item/Field/DBItemFieldDate.view.editField.php
+++ classes/DB/Item/Field/DBItemFieldDate.view.editField.php
@@ -11,4 +11,6 @@ else {
 	$value = "";
 }
 
-?><input type="date" format="<?php echo $this->html($this->editFormat);?>" name="<?php echo $this->getPostName($args);?>" value="<?php echo $this->html($value);?>">
+$required = $this->null? "": " required=\"required\"";
+
+?><input type="date" format="<?php echo $this->html($this->editFormat);?>" name="<?php echo $this->getPostName($args);?>" value="<?php echo $this->html($value);?>"<?php echo $required;?>>
